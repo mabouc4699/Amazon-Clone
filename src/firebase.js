edf8143a7e8a5b5033f9c789330111c1b2f563a9
Matthew Bouchard <matthewbouchard72@outlook.com>
2022-01-10 14:31:48 -0500

    Edited firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
+require("dotenv").config();
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-8REpFhDgdpXzOoo9mrtNZlD7A-QBBmg",
-  authDomain: "clone-b3039.firebaseapp.com",
-  projectId: "clone-b3039",
-  storageBucket: "clone-b3039.appspot.com",
-  messagingSenderId: "499539256736",
-  appId: "1:499539256736:web:77e9a747287fe07466b0c5",
-  measurementId: "G-1Z5S68V2ND",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
